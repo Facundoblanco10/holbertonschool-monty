febdeedb7ed0dc7daae2a51c213ac26223892977
MichaelTwG <michaelgolenia1@gmail.com>
2022-08-16 13:50:51 -0700

    i try to do the push function

--- micha/main.c
+++ micha/main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  *
  *
@@ -13,15 +14,9 @@ int main (int argc, char * argv[])
 	char buf[256];
 	char *token1;
 	char *token2;
-	char *buf;
 	char *buf_dup;
+	stack_t **head = NULL;
 	(void)argc;
-
-	instruction_t functions[] = {
-		{"push", push()}
-		{"pall", pall()}
-		{"\0", NULL}
-	};
 
 	if (!argv[1])
 	{
@@ -38,23 +33,58 @@ int main (int argc, char * argv[])
 	}
 	while (fgets(buf, len, name_of_file) != NULL)
 	{
-		token1 = strtok(buf_dup, " \t");
-		token2 = strtok(NULL, " \t");
-		if (token2[0] != '0')
-			value = atoi(token2);
-/*
-		while (functions[i] != NULL)
-		{
-			if(strcmp(functions[i][0], token1) == 0)
-			{
-				ejecutar la funcion correspondiente
-			}
-		}
-		printf("L%d: unknown instruction %s", line, token1);
-*/
+		buf[256] = '\0';
+		match_function(buf, line);
 		line++;
 	}
 	fclose(name_of_file);
 
 	return (0);
+}
+
+void match_function(char *buf, int line)
+{
+	char buf_dup = strdup(buf);
+	char *token1 = strtok(buf_dup, " \t");
+	instruction_t functions[] = {
+		{"push", push()}
+		{"pall", pall()}
+		{"\0", NULL}
+	};
+
+	while (functions[i] != NULL)
+	{
+		if(strcmp(functions[i].opcode, token1) == 0)
+		{
+			functions[i].f(head, buf);
+			return;
+		}
+	}
+	printf("L%d: unknown instruction %s", line, token1);
+
+}
+void push(stack_t **head, char *buf)
+{
+	char buf_dup = strdup(buf);
+	char *token1 = strtok(buf_dup, " \t");
+	char *token2 = strtok(NULL, " \t");
+	int value = atoi(token2);
+	stack_t *n_node = malloc(sizeof(stack_t));
+
+	if (!n_node)
+	{
+		printf("Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	n_node->n = value;
+	n_node->next = NULL;
+	n_node->prev = NULL;
+	if (*head = NULL)
+	{
+		*head = n_node;
+		return;
+	}
+	(*head)->prev = n_node;
+	n_node->next =  (*head);
+
 }
